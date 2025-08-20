e4d7941e103d2550fba79e6253fd8e82c41e0ff5
Piyush Nimbalkar <piyushnimbalkar19@gmail.com>
2025-08-20 12:56:39 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 
 const clerkPubKey = 
-"pk_test_c3VwZXItc3RhbGxpb24tODQuY2xlcmsuYWNjb3VudHMuZGV2JA"; // use your real key
+"pk_test_*****************************************************"; // use your real key
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
